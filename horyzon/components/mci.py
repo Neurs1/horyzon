c43819d9f36e61d76492f4d91f5ddd5bb1edf4ff
Neurs <31346263+Neurs1@users.noreply.github.com>
2022-01-19 10:54:07 +0700

    Update mci.py

--- horyzon/components/mci.py
+++ horyzon/components/mci.py
@@ -6,6 +6,7 @@ from threading import Thread
 from nextcord import Embed, Colour
 
 async def run(stdout, ctx):
+  #Get skin and previous names
   global down_skin, view
   uuid = loads(stdout)["id"]
   username = loads(stdout)["name"]
@@ -32,13 +33,18 @@ async def checkspec(string):
   if string.isalnum():
     return True
   else:
+    #If contains special characters, remove "_" and check again, if False again, return False.
     string = string.replace("_", "")
     if string.isalnum() == False:
       return False
     else:
       return True
 
+#Condition:
+#Minecraft username length is 3 - 16
+#No special characters, except "_"
 async def Mci(ctx, name):
+  #Check the requirement
   if len(name) < 16 and await checkspec(name):
     try:
       proc = await asyncio.create_subprocess_shell(
@@ -49,6 +55,7 @@ async def Mci(ctx, name):
       t = Thread(target = await run(stdout[0].decode(), ctx))
       t.start()
     except:
+      #If API return nothing, that mean the name is not taken.
       await ctx.send(embed = Embed(title = "Username available!", description = "This username hasn't been owned yet, go grab it before someone does!", color = Colour.green()))
   else:
     await ctx.send(embed = Embed(title = "Invalided username", description = "This username contains more than 16 characters or contains special characters!", color = Colour.red()))
